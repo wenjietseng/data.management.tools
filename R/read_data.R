2dacf2fae23f8ad73de46cf18332605f6b9176d6
wen-jie.tseng <wenjietseng@gmail.com>
2020-09-20 15:08:39 +0200

    typo

--- R/read_data.R
+++ R/read_data.R
@@ -9,7 +9,7 @@
 #' @export
 #'
 #' @examples
-read_dta <- function(path_to_data, separator = ",", use_header = TRUE) {
+read_data <- function(path_to_data, separator = ",", use_header = TRUE) {
   file_names <- paste0(path_to_data, dir(path_to_data))
   dta <- lapply(file_names, function(x) fread(x, sep = separator, header = use_header))
   dta <- as.data.frame(rbindlist(dta))
